29ec27a122d6577c1de35e342eeb07ef13d41e57
Maria Luiza Linhares Dantas <mlldantas@pm.me>
2018-11-06 20:37:18 -0300

    my installation script first commit

---
+++ install_my_stuff.sh
@@ -0,0 +1,12 @@
+echo ""
+echo " Installing my crap!"
+echo " 1st: Updating files"
+apt-get update
+
+echo " SLACK"
+sudo snap install slack --classic
+
+echo ""
+echo " After installing Anaconda"
+echo " Install these Python packages"
+
